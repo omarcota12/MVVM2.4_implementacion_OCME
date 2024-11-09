203023c56af052514b0c79786c4e77d0e6ccadd0
omarcotamedina28@gmail.com <omarcotamedina18@gmail.com>
2024-11-08 19:11:00 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_OCME/vistaModelo/VMpagina1.cs
@@ -0,0 +1,78 @@
+﻿using MvvmGuia.VistaModelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM_implementacion_OCME.vistaModelo
+{
+    public class VMpagina1 : BaseViewModel
+    {
+        #region VARIABLES
+        string _N1;
+        string _N2;
+        string _R;
+        string _TipoUsuario;
+        #endregion
+        #region CONTRUCTOR
+        public VMpagina1(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+        #region OBJETOS
+        public string TipoUsuario
+        {
+            get { return _TipoUsuario; }
+            set { SetValue(ref _TipoUsuario, value);}
+        }
+        public string SeleccionarTipoUsuario
+        {
+            get { return _TipoUsuario; }
+            set { SetValue(ref _TipoUsuario, value); 
+            TipoUsuario = _TipoUsuario;
+            }
+        }
+        public string N1
+        {
+            get { return _N1; }
+            set { SetValue(ref _N1, value); }
+        }
+        public string N2
+        {
+            get { return _N2; }
+            set { SetValue(ref _N2, value); }
+        }
+        public string R
+        {
+            get { return _R; }
+            set { SetValue(ref _R, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public void Sumar()
+        {
+            double n1 = 0;
+            double n2 = 0;
+            double r = 0;
+
+            n1 = Convert.ToDouble(N1);
+            n2 = Convert.ToDouble(N2);
+            r = Convert.ToDouble(R);
+
+            r = n1 + n2;
+            R = r.ToString();
+        }
+        public async Task ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand ProcesoSimplecommand => new Command(async () => await ProcesoSimple());
+        public ICommand Sumarcommand => new Command(Sumar);
+        #endregion
+    }
+}
